316f0cdf86ea47ca5e144664f6e83c0418bdc66d
Dominik <megakopec@gmail.com>
2020-06-03 11:26:37 -0400

    src: add logger and dateUtil to app.js for testing

--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
 const express = require("express")
+const logger = require("./util/logger.util")
+const dateUtil = require("./util/date.util")
+
 const app = express()
 const port = 3000
 
-app.get("/", (req, res) => res.send("Hello World!"))
+app.get("/", (req, res) =>(
+  logger.info(`Request to / at ${dateUtil.format(Date.now())}`),
+  res.send("Welcome to the Planets API"))
+)
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () => (
+  logger.info(`Starting server on port : ${port}`),
+  logger.info(dateUtil.format(Date.now())))
+)
