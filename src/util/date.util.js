e0b1bf69a4fc71e951a03f3d6d3892fa1cf7749c
Dominik <megakopec@gmail.com>
2020-06-16 20:46:58 -0400

    util: add js doc

--- src/util/date.util.js
+++ src/util/date.util.js
@@ -1,6 +1,13 @@
-const moment = require("moment")
+const moment = require('moment')
 
 class DateUtil{
+  /**
+   *
+   * @param {string} value
+   * @param {string}format
+   * @returns {string}
+   * @description Formats date.
+   */
   static format (value, format) {
     return (value && moment(value).utc(true).format(format || "LLLL"))
   }
