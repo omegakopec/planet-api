e0b7f27389a3b1948e60ed643f1d84ff596af163
omegakopec <megakopec@gmail.com>
2020-08-12 12:42:00 -0400

    schema: update planet schema with new fields

--- src/schema/planet.schema.js
+++ src/schema/planet.schema.js
@@ -1,13 +1,20 @@
 const mongoose = require('mongoose');
 
-//Testing this simple schema, will add more soon
 const PlanetSchema = mongoose.Schema({
   name: {
     type: String,
     required: true
   },
   number_of_moons: Number,
-  mass: String
+  mass: String,
+  density: String,
+  star_name: String,
+  average_distance: Number,
+  perihelion: String,
+  aphelion: String,
+  average_temperature: Number,
+  temperature_high: Number,
+  temperature_low: Number,
 });
 
 module.exports = mongoose.model("Planets", PlanetSchema);
